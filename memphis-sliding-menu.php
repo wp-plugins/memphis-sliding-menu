ed8fcef0feb3d6610662ffeb2f5c765e978c680f
bhaldie <bhaldie@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-26 18:21:33 +0000

    [ Release ][ Version 1.0.2 ] A sliding menu widget for all your  WordPress pages.  This plugin allows you to: * Choose which pages you want to display * Place the widget on any sidebar as many times as needed.
    git-svn-id: https://plugins.svn.wordpress.org/memphis-sliding-menu/tags/1.0.2@882502 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ memphis-sliding-menu.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ 
+Plugin Name: Memphis Sliding Menu
+Plugin URI: http://
+Description: A sliding menu for WordPress
+Author: Ian Howatson
+Version: 1.0.2
+Author URI: http://www.kingofnothing.net/
+Date: 3/26/2013
+
+Copyright 2013 Ian Howatson  (email : ian.howatson@kingofnothing.net)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+include 'mslide-settings.php';
+include 'mslide-functions.php';
+include 'mslide-shortcode.php';
+add_action( 'widgets_init', 'memphis_sliding_menu_widget' );
+add_action( 'wp_enqueue_scripts', 'memphis_sliding_menu_script' );
+add_action( 'admin_enqueue_scripts', 'memphis_sliding_menu_script' );
+add_action('admin_init', 'mslide_admin');
+
+?>
---
+++ mslide-shortcode.php
@@ -0,0 +1,38 @@
+<?php
+
+function mslide_list_children_shortcode($att, $content=null) { mslide_list_children($att); }
+add_shortcode( 'mslide_list_children', 'mslide_list_children_shortcode' );
+
+function mslide_list_children($att) {
+	global $post;
+	if(isset($att['pageid'])) $pageid = $att['pageid'];
+	else $pageid = $post->ID;
+	if ( is_user_logged_in() ) $post_status = 'publish,private';
+	else $post_status = 'publish';
+	$args =  array(
+		'sort_column' => 'menu_order',
+		'child_of' => $pageid,
+		'post_status' => $post_status
+	);
+	$children = get_pages($args);
+	?>
+	<table class="mslide-table">
+		<tr>
+			<th>Page Title</th>
+			<th></th>
+		</tr>
+		<?php
+		$mod = 0;
+		foreach($children as $index => $child) {
+			$permalink = get_permalink( $child->ID );
+			//var_dump($permalink);
+			if($mod == 0 ) { $class = 'mslide-light-bg'; $mod = 1; }
+			else { $class = 'mslide-dark-bg'; $mod = 0; }
+			echo '<tr><td class="'.$class.'">'.$child->post_title.'</td>';
+			echo '<td class="mslide-view-page '.$class.'"><a href="'.$permalink.'" >View Page</a></td></tr>';
+		}
+		?>
+	</table>
+	<?php
+}
+?>
